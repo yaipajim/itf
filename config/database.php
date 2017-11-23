21a27731327b7aec874fe160a1fe30201e4f3c07
yaipajim <pimmiqx@pimmiqxs-MBP.it.kmitl.ac.th>
2017-11-23 23:22:58 +0700

    seddheerr

--- config/database.php
+++ config/database.php
@@ -41,8 +41,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '33060'),
+            'host' => env('DB_HOST', localhost),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
